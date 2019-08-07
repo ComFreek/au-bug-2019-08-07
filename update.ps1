2980c47fb7f67ccd9e442510eb08d82b8a0e23c0
ComFreek <comfreek@outlook.com>
2019-08-07 12:24:16 +0200

    First commit

---
+++ update.ps1
@@ -0,0 +1,25 @@
+Import-Module au
+
+function global:au_AfterUpdate ($Package)  {
+	Set-DescriptionFromReadme $Package
+}
+
+function global:au_SearchReplace {
+	@{
+		'au_bug.nuspec' = @{
+			"<version>[^<]*</version>" = "<version>$($Latest.version)</version>";
+			"<releaseNotes>[^<]*</releaseNotes>" = "<releaseNotes>$($Latest.releaseNotesUrl)</releaseNotes>"
+		}
+	}
+}
+
+
+function global:au_GetLatest {
+	return @{
+		version          = '1.0.0';
+		releaseNotesUrl  = "https://example.com/?random-here=$(Get-Random)";
+		url32            = 'https://github.com/majkinetor/au/archive/master.zip'
+	}
+}
+
+Update-Package -ChecksumFor None
